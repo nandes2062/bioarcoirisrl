08d24994598552483b49017608e2e3662caa92df
Fernando Averanga <nandes.ingsistemas@gmail.com>
2023-03-26 01:45:02 -0400

    FIX: reload pages news base

--- pages/catalogo/index.vue
+++ pages/catalogo/index.vue
@@ -3,13 +3,68 @@
     <section class="bg-gray-100 py-12">
       <div class="container max-w-screen-xl mx-auto px-4">
         <h2 class="text-3xl font-bold mb-8">NUESTROS PRODUCTOS</h2>
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <ContentList
+          path="/catalogo"
+          :query="{
+            only: ['title', 'description', 'tags', '_path', 'img'],
+            where: {
+              tags: {
+                $contains: filter,
+              },
+            }
+          }"
+        >
+          <!-- Default list slot -->
+          <template v-slot="{ list }">
+            <nuxt-link class="hidden" v-for="item in list" :to="item._path">{{ item.title }}</nuxt-link>
+          </template>
+          <!-- Not found slot to display message when no content us is found -->
+          <template #not-found>
+            <p>No hay articulos para mostrar.</p>
+          </template>
+        </ContentList>
+        <div class="mb-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           <Card
-            v-for="item in data"
+            v-for="item in data.products"
             @update="updateCard"
             :product="item" >
           </Card>
         </div> <!-- grid .// -->
+        <div class="flex justify-center pb-14">
+          <button
+            @click="pageBack"
+            class="border rounded-lg flex justify-center items-center px-4"
+          >
+            <svg width="16" height="16" viewBox="0 0 16 16" style="fill: #7fcd18">
+              <path
+                d="M9.23104082,14.9514887 L8.43827216,15.7648617 C8.10259533,16.1092629 7.55979877,16.1092629 7.22769298,15.7648617 L0.285610623,8.6460158 C-0.0500661988,8.30161461 -0.0500661988,7.74471056 0.285610623,7.40397321 L7.22769298,0.281463477 C7.5633698,-0.0629377142 8.10616636,-0.0629377142 8.43827216,0.281463477 L9.23104082,1.0948365 C9.57028867,1.44290153 9.56314661,2.01079712 9.2167567,2.35153446 L4.91366553,6.5576256 L15.1768058,6.5576256 C15.6517528,6.5576256 16.033853,6.94965674 16.033853,7.43694779 L16.033853,8.60937738 C16.033853,9.09666842 15.6517528,9.48869957 15.1768058,9.48869957 L4.91366553,9.48869957 L9.2167567,13.6947907 C9.56671764,14.0355281 9.5738597,14.6034236 9.23104082,14.9514887 Z"
+              ></path>
+            </svg>
+          </button>
+          <form @submit.prevent="handlePage" class="mx-4">
+            <label for="">Página</label>
+            <input
+              placeholder="1"
+              v-model.number="page"
+              class="w-20 mx-1 text-center bg-transparent shadow appearance-none border rounded-lg py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+            />
+            <label for="">de {{ data.totalPages }}</label>
+          </form>
+          <button @click="pageNext" class="border rounded-lg flex justify-center items-center px-4">
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              class="transform rotate-180"
+              style="fill: #7fcd18"
+            >
+              <path
+                d="M9.23104082,14.9514887 L8.43827216,15.7648617 C8.10259533,16.1092629 7.55979877,16.1092629 7.22769298,15.7648617 L0.285610623,8.6460158 C-0.0500661988,8.30161461 -0.0500661988,7.74471056 0.285610623,7.40397321 L7.22769298,0.281463477 C7.5633698,-0.0629377142 8.10616636,-0.0629377142 8.43827216,0.281463477 L9.23104082,1.0948365 C9.57028867,1.44290153 9.56314661,2.01079712 9.2167567,2.35153446 L4.91366553,6.5576256 L15.1768058,6.5576256 C15.6517528,6.5576256 16.033853,6.94965674 16.033853,7.43694779 L16.033853,8.60937738 C16.033853,9.09666842 15.6517528,9.48869957 15.1768058,9.48869957 L4.91366553,9.48869957 L9.2167567,13.6947907 C9.56671764,14.0355281 9.5738597,14.6034236 9.23104082,14.9514887 Z"
+              ></path>
+            </svg>
+          </button>
+        </div>
       </div>
     </section>
   </div>
@@ -18,15 +73,37 @@
   import type { ParsedContent } from '@nuxt/content/dist/runtime/types'
   import Card from '@/common/components/card/index.vue'
   import { cartStore } from '@/store/cart'
+  import { useCatalogo } from './catalogo.composable'
   interface ProductContent extends ParsedContent {
     quantity: number
   }
   const cartData = cartStore()
+  // get tag query
+  const {
+    query: { tags },
+  } = useRoute();
+
+  const filter = ref(tags?.split(','));
+  const route = useRoute()
+  const router = useRouter()
+  const page = ref(1)
+
   let { data } = await useAsyncData('catalogo', async () => {
+    let prodTotal: object[]
     if (cartData.cart.length <= 0) {
-      return await queryContent<ProductContent>('/catalogo/').find()
+      prodTotal = await queryContent<ProductContent>('/catalogo/').find()
     } else {
-      return cartData.cart
+      prodTotal = cartData.cart
+    }
+    const itemsForPage = 8
+    const totalItems = prodTotal.length
+    const totalPages = Math.ceil(totalItems / itemsForPage)
+    const limitBefore = (page.value - 1) * itemsForPage
+    const limitAfter = ((page.value - 1) * itemsForPage) + (itemsForPage - 1)
+    const products = prodTotal.filter((v, k) => k >= limitBefore && k <= limitAfter)
+    return {
+      products,
+      totalPages
     }
   })
 
@@ -39,4 +116,5 @@
       }
     })
   }
+  const { handlePage, pageNext, pageBack } = useCatalogo({ route, router, newsReponse: data, page })
 </script>
--- pages/noticias/[...slug].vue
+++ pages/noticias/[...slug].vue
@@ -4,69 +4,58 @@
 const { path } = useRoute();
 const { data } = await useAsyncData(`content-${path}`, async () => {
   // fetch document where the document path matches with the cuurent route
-  let article = queryContent().where({ _path: path }).findOne();
   // get the surround information,
   // which is an array of documeents that come before and after the current document
-  let surround = queryContent().only(["_path", "title", "description"]).sort({ date: 1 }).findSurround(path);
+  // let surround = queryContent().only(["_path", "title", "description"]).sort({ date: 1 }).findSurround(path);
 
   return {
-    article: await article,
-    surround: await surround,
+    article: await queryContent(path).findOne(),
+    // surround: await surround
   };
 });
 
-// destrucure `prev` and `next` value from data
-const [prev, next] = data.value.surround;
-console.log({ data, prev, next });
-
 // set the meta
-useHead({
-  title: data.value.article.title,
-  meta: [
-    { name: "description", content: data.value.article.description },
-    {
-      hid: "og:image",
-      property: "og:image",
-      content: `https://site.com/${data.value.article.img}`,
-    },
-  ],
-});
+// useHead({
+//   title: data.value.article.title,
+//   meta: [
+//     { name: "description", content: data.value.article.description },
+//     {
+//       hid: "og:image",
+//       property: "og:image",
+//       content: `https://site.com/${data.value.article.img}`,
+//     },
+//   ],
+// });
 </script>
 <template>
-  <main id="main" class="article-main">
-    <header v-if="data.article" class="article-header">
-      <div class="img-cont h-72 mb-12">
-        <img :src="`/${data.article.img}`" :alt="data.article.title" class=" rounded-2xl" />
-      </div>
-      <h1 class="heading">{{ data.article.title }}</h1>
-      <p class="supporting">{{ data.article.description }}</p>
-      <ul class="article-tags">
-        <li class="tag" v-for="(tag, n) in data.article.tags" :key="n">{{ tag }}</li>
-      </ul>
-    </header>
-    <hr />
-    <section class="article-section">
-      <aside class="aside">
-        <!-- Toc Component -->
-        <Toc :links="data.article.body.toc.links" />
-      </aside>
-      <article class="article">
-        <!-- render document coming from query -->
-        <ContentRenderer :value="data.article">
-          <!-- render rich text from document
-          <MarkdownRenderer :value="data.article" /> -->
-
-          <!-- display if document content is empty -->
-          <template #empty>
-            <p>No content found.</p>
-          </template>
-        </ContentRenderer>
-      </article>
-    </section>
-
-    <!-- PrevNext Component -->
-    <PrevNext :prev="prev" :next="next" />
-  </main>
+    <!-- component with nested async dependencies -->
+    <main id="main" class="article-main">
+      <!-- <code>{{ data.article }}</code> -->
+      <header class="article-header">
+        <div class="img-cont h-72 mb-12">
+          <img :src="`/${data.article.img}`" :alt="data.article.title" class=" rounded-2xl" />
+        </div>
+        <h1 class="heading">{{ data.article.title }}</h1>
+        <p class="supporting">{{ data.article.description }}</p>
+        <ul class="article-tags">
+          <li class="tag" v-for="(tag, n) in data.article.tags" :key="n">{{ tag }}</li>
+        </ul>
+      </header>
+      <hr />
+      <section class="article-section">
+        <aside class="aside">
+          <Toc :links="data.article.body.toc.links" />
+        </aside>
+        <article class="article">
+          <ContentRenderer :value="data.article">
+            <template #empty>
+              <p>No content found.</p>
+            </template>
+          </ContentRenderer>
+        </article>
+      </section>
+      <!-- <PrevNext :prev="data.surround[0]" :next="data.surround[1]" /> -->
+    </main>
 </template>
 
 <style scoped>
